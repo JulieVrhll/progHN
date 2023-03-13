d90d99c1934c6e63db7ce352a49080079b7f99d3
JulieVrhll <124155511+JulieVrhll@users.noreply.github.com>
2023-03-13 14:29:46 +0100

    V5
    ajout de devoirarray.js

---
+++ assets/JS/devoirarray.js
@@ -0,0 +1,40 @@
+function exercice1() {
+let desserts = ["Tarte", "Croissant", "Brioche", "Cookie"];
+	let suppr_last = desserts.pop();
+	let add_first = desserts.unshift(suppr_last);	
+	document.getElementById("exercice1Resultat").innerHTML = desserts;
+}
+
+
+function exercice2() {
+	let text = document.getElementById("texteExercice2").value;
+    text = text.replace(/[.,\"]/g,"");
+    text = text.replace(/\'/g," ");
+    let mots = text.split(" ");
+    let maj = text.toUpperCase();
+    document.getElementById("exercice2Resultat").innerHTML = maj;
+}
+
+
+function exercice3() {
+	let text = document.getElementById("texteExercice3").value;
+	text = text.replace(/[.,\"]/g,"");
+    text = text.replace(/\'/g," ");
+	let mots = text.split(" ");
+	let motsFiltre = mots.filter(mot => mot.length > 3);
+	let result = motsFiltre.join(" ");
+	document.getElementById("exercice3Resultat").innerHTML = result;
+}
+
+
+function exercice4() {
+	let text = document.getElementById("texteExercice4").value;
+	text = text.replace(/[.,\"]/g,"");
+    text = text.replace(/\'/g," ");
+	let mots = text.split(" ");
+	const tableau = [];
+	mots.forEach(function(mot) {
+		tableau.push(mot);
+});
+	document.getElementById("exercice4Resultat").innerHTML = tableau;
+}
